444b4895560c331c29e55bdcb6ff6b7200eb9635
Gerry Leonard <leonardger@gmail.com>
2015-02-02 18:28:05 +0000

    zen

--- js/app.js
+++ js/app.js
@@ -1,70 +1,89 @@
-$(document).ready(function() {
-    $('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
+$(init);
+function init() {
+    $('.ryu').mouseenter(showReady);
+    $('.ryu').mouseleave(showStill);
+    $('.ryu').mousedown(showCool);
+    $('.ryu').mouseup(backReady);
+    $(document).keydown(fire);
+     $(document).keyup(postFire);
+
+
+  } //end init
+
+  function showReady(){
+     $('.ryu-still').hide();
     $('.ryu-cool').hide(); 
     $('.ryu-ready').show();
-  })
-  .mouseleave(function() {
-    $('.ryu-ready').hide();
+    $('#instructions').hide().html('Click X to fire!').fadeIn ("slow");
+  }
+
+  function showStill(){
+     $('.ryu-ready').hide();
     $('.ryu-cool').hide(); 
     $('.ryu-still').show();
-  })
+    $('#instructions').hide().html('Click Mouse on Ryu to strike a pose.').fadeIn ("slow");
+  }
 
-
-  
- .mousedown(function() {
-
+  function showCool(){
     $('.ryu-ready').hide();
     $('.ryu-throwing').hide();
-        $('.ryu-still').hide(); 
-        $('.ryu-cool').show(); 
-  })
-  .mouseup(function() {
-    $('.ryu-ready').show();
-        $('.ryu-still').hide(); 
-        $('.ryu-cool').hide(); 
-  })
+    $('.ryu-still').hide(); 
+    $('.ryu-cool').show(); 
+    $('#instructions').hide().html('Ryu, The Zen Warrior!').fadeIn ("slow");
+  }
+
+  function backReady(){
+    showReady() 
+  }
+
+  function fire(){
+    var keycode = (event.which);
+      if(keycode == '88')
+      {
+      playHadouken();    
+      $('.ryu-ready').hide();
+      $('.ryu-throwing').show();
+      $('.ryu-cool').hide(); 
+      $('#instructions').hide();
+      $('.hadouken').finish().show()
+      .animate
+            (
+              {'left': '300px'},
+              500,
+              function() 
+              {
+                $(this).hide();
+                $(this).css('left', '-212px');
+              }
+
+            )
+        }
+    }
+
+    function postFire(){
+         $('.ryu-cool').hide();
+          $('.ryu-ready').show(); 
+          $('.ryu-still').hide(); 
+          $('.ryu-throwing').hide();
+          $('#instructions').hide().html('Click Mouse on Ryu to strike a pose.').fadeIn ("slow");
+    }
 
 
-});
 
-    $(document).keydown(function(e) {
-
-       var keycode = (event.which);
-    if(keycode == '88'){
-    playHadouken();    
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
-    $('.ryu-cool').hide(); 
-    $('.hadouken').finish().show()
-    .animate(
-      {'left': '300px'},
-      500,
-      function() {
-        $(this).hide();
-        $(this).css('left', '-212px');
-      }
-
-  );
-}
-
-$(document).keyup(function(){
-  $('.ryu-cool').hide();
-  $('.ryu-ready').show(); 
-  $('.ryu-still').hide(); 
-  $('.ryu-throwing').hide();
-
-});
-
-
- 
-
-
-function playHadouken () {
+  function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
 
 
- });
+    
+
+    
+  
+
+
+ 
+
+
+
